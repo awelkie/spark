4341ee021a41fd21bce36ea6c41b6f65268a8039
Allen Welkie <allen.welkie@gmail.com>
2018-05-08 16:01:44 -0400

    Added script to generate synthetic KMeans data

---
+++ data/mllib/create_kmeans_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+""" Creates artifical k-means data. """
+
+import numpy
+
+CLUSTERS = [
+    [0] * 10,
+    [2] * 10,
+]
+CLUSTER_DIMS = 100
+NUM_CLUSTERS = 10
+NUM_POINTS_PER_CLUSTER = 1_000
+
+def main():
+    clusters = [[i] * CLUSTER_DIMS for i in range(NUM_CLUSTERS)]
+    points = numpy.concatenate([
+        numpy.random.normal(loc=cluster, scale=100, size=(NUM_POINTS_PER_CLUSTER, len(cluster)))
+        for cluster in clusters])
+    numpy.savetxt('kmeans_data.txt', points)
+
+if __name__ == '__main__':
+    main()
